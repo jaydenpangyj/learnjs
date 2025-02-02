b18d83dc78163f0407ab173110479f1b81282c45
Jayden Pang <jaydenpangyunjae@icloud.com>
2025-02-02 13:05:51 +0800

    let var for js

---
+++ jssyntax.js
@@ -0,0 +1,18 @@
+let students = 200;
+let teacher = 10;
+let class_size = students / teacher;
+console.log(class_size);
+const jayden_nric = "1234A";
+// jayden_nric="2456A"
+var name_a = "potato";
+var name_b = "beans";
+var name_c = "apple";
+var a_books = 34;
+var b_books = 83;
+var c_books = 92;
+var x = 0;
+for (let i = 0; i < 10; i++) {
+    console.log(name_a + " has " + a_books);
+    console.log(name_b + " has " + b_books);
+    console.log(name_c + "has " + c_books);
+}
